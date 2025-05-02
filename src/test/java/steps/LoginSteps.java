2520da4c37ababc125a6f3f50cd3ea73df9401a4
Rafael Horochovec <rl.horochovec@gmail.com>
2025-05-02 12:16:25 -0300

    Allure report

--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -3,6 +3,8 @@ package steps;
 import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.Então;
 import io.cucumber.java.pt.Quando;
@@ -14,12 +16,16 @@ public class LoginSteps {
     private Playwright playwright;
     private Browser browser;
 
-    @Dado("que inseri usuário {string} e senha {string}")
-    public void que_inseri_usuário_e_senha(String usuario, String senha) {
+    @Before
+    public void setup() {
         playwright = Playwright.create();
         browser = playwright.chromium().launch();
         page = browser.newPage();
         page.navigate("https://rlhorochovec.github.io/qa/");
+    }
+
+    @Dado("que inseri usuário {string} e senha {string}")
+    public void que_inseri_usuário_e_senha(String usuario, String senha) {
         page.fill("#login", usuario);
         page.fill("#senha", senha);
     }
@@ -33,6 +39,10 @@ public class LoginSteps {
     public void apresentará_uma_mensagem_de(String mensagem) {
         String msgErro = page.locator("#erro").textContent();
         assertEquals(mensagem, msgErro);
+    }
+
+    @After
+    public void close() {
         browser.close();
         playwright.close();
     }
